bc6db2b6083c135c194b9040f117e48d774965cd
Vikaspathakg <pathakvikas034@gmail.com>
2024-07-12 20:57:41 +0530

    first commit

--- present.java
+++ present.java
@@ -2,7 +2,8 @@ package Arrays;
 import java.util.Scanner;
 public class present {
 
-    // Qus: Sort an Array consisting of only 0 and 1!!
+    // Qus: Solve the below question using two pointers!!
+
 
     static void printArray(int[] arr){
         for(int i=0;i<arr.length;i++){
@@ -20,22 +21,19 @@ public class present {
 
     static void sortZeroesAndOne(int[] arr){
         int n = arr.length;
-        int zeroes = 0;
-        // Count Number of Zeroes
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                zeroes++;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(i<zeroes){
-                arr[i]=0;
-            }
-            else{
-                arr[i]=1;
+        int left = 0;
+        int right = n-1;
+
+        while(left<right){
+            if(arr[left]==1 && arr[right]==0){
+                swapArray(arr,left,right);
+                left++;
+                right--;
             }
         }
     }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Array size :");
@@ -55,6 +53,59 @@ public class present {
 
     }
 }
+
+// Qus: Sort an Array consisting of only 0 and 1!!
+//static void printArray(int[] arr){
+//    for(int i=0;i<arr.length;i++){
+//        System.out.println(arr[i]+" ");
+//    }
+//    System.out.println();
+//}
+//
+//static void swapArray(int[] arr,int i,int j){
+//    int temp = arr[i];
+//    arr[i] = arr[j];
+//    arr[j] = temp;
+//
+//}
+//
+//static void sortZeroesAndOne(int[] arr){
+//    int n = arr.length;
+//    int zeroes = 0;
+//    // Count Number of Zeroes
+//    for(int i=0;i<n;i++){
+//        if(arr[i]==0){
+//            zeroes++;
+//        }
+//    }
+//    for(int i=0;i<n;i++){
+//        if(i<zeroes){
+//            arr[i]=0;
+//        }
+//        else{
+//            arr[i]=1;
+//        }
+//    }
+//}
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter Array size :");
+//    int n = sc.nextInt();
+//    int[] arr = new int[n];
+//
+//    System.out.println("Enter "+n+" elements :");
+//    for(int i=0;i<arr.length;i++){
+//        arr[i] = sc.nextInt();
+//
+//    }
+//    System.out.println("Original Array");
+//    printArray(arr);
+//    sortZeroesAndOne(arr);
+//    System.out.println("Sorted Array :");
+//    printArray(arr);
+//
+//}
+//}
 
 
 
