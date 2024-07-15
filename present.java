04c098cfbf786f000de3a542dbf570c696c8f613
Vikaspathakg <pathakvikas034@gmail.com>
2024-07-15 15:02:08 +0530

    first commit

--- present.java
+++ present.java
@@ -2,7 +2,7 @@ package Arrays;
 import java.util.Scanner;
 public class present {
 
-    // Qus: Solve the below question using two pointers!!
+    // Qus: Given an Array of integers 'a' move all the even integers at the beginning of the Array followed by the all the odd integers. The relative order of odd or even integers does not matter. Return any Array that Satisfies the condition!!
 
 
     static void printArray(int[] arr){
@@ -19,18 +19,25 @@ public class present {
 
     }
 
-    static void sortZeroesAndOne(int[] arr){
+    static void sortArrayByParity(int[] arr){
         int n = arr.length;
         int left = 0;
         int right = n-1;
 
         while(left<right){
-            if(arr[left]==1 && arr[right]==0){
+            if(arr[left]%2==1 && arr[right]%2==0){
                 swapArray(arr,left,right);
                 left++;
                 right--;
             }
+            if(arr[left]%2==0){
+                left++;
+            }
+            if(arr[right]%2==1){
+                right--;
+            }
         }
+
     }
 
 
@@ -47,12 +54,70 @@ public class present {
         }
         System.out.println("Original Array");
         printArray(arr);
-        sortZeroesAndOne(arr);
+        sortArrayByParity(arr);
         System.out.println("Sorted Array :");
         printArray(arr);
 
     }
 }
+
+
+// Qus: Solve the below question using two pointers!!
+
+//static void printArray(int[] arr){
+//    for(int i=0;i<arr.length;i++){
+//        System.out.println(arr[i]+" ");
+//    }
+//    System.out.println();
+//}
+//
+//static void swapArray(int[] arr,int i,int j){
+//    int temp = arr[i];
+//    arr[i] = arr[j];
+//    arr[j] = temp;
+//
+//}
+//
+//static void sortZeroesAndOne(int[] arr){
+//    int n = arr.length;
+//    int left = 0;
+//    int right = n-1;
+//
+//    while(left<right){
+//        if(arr[left]==1 && arr[right]==0){
+//            swapArray(arr,left,right);
+//            left++;
+//            right--;
+//        }
+//    }
+//}
+//
+//
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter Array size :");
+//    int n = sc.nextInt();
+//    int[] arr = new int[n];
+//
+//    System.out.println("Enter "+n+" elements :");
+//    for(int i=0;i<arr.length;i++){
+//        arr[i] = sc.nextInt();
+//
+//    }
+//    System.out.println("Original Array");
+//    printArray(arr);
+//    sortZeroesAndOne(arr);
+//    System.out.println("Sorted Array :");
+//    printArray(arr);
+//
+//}
+//}
+
+
+
+
+
+
 
 // Qus: Sort an Array consisting of only 0 and 1!!
 //static void printArray(int[] arr){
